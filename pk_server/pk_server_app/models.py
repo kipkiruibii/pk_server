fae7408a841ea3e96006a55049946bbafa6386bb
kipkiruibii <biihezron1@gmail.com>
2023-12-24 11:35:49 +0300

    sec commit

--- pk_server/pk_server_app/models.py
+++ pk_server/pk_server_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime, timezone
+import pytz
 
-# Create your models here.
+
+class CustomerDetails(models.Model):
+    phone_number = models.TextField(default='Phone Number')
+    email_address = models.TextField(default='Email Address')
+    date_registered = models.DateTimeField(default=datetime.now(pytz.timezone('Africa/Nairobi')))
+
+    def __str__(self):
+        return self.phone_number
+
